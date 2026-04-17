510e5c47b93155f3f13d88f293d83dd689bb9990
Alinne Oliveira <alinnealvesoliveira@gmail.com>
2026-04-16 21:38:51 -0300

    fix: declaracoes de tipo css e limpeza final do relatorio

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,5 @@
 /// <reference types="vite/client" />
-
+declare module "*.css";
 /**
  * Definição das Variáveis de Ambiente do Vite.
  * Garante o preenchimento automático (autocomplete) e a segurança de tipos 
