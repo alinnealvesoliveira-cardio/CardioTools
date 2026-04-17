7012d605a75563261ccb6835c5941a8836557d27
Alinne Oliveira <alinnealvesoliveira@gmail.com>
2026-04-16 21:56:56 -0300

    fix: removendo erros de tipo no main e limpando css

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App'; // Removi a extensão .tsx (é uma boa prática)
+import App from './App'; 
+
+// @ts-ignore
 import './index.css';
 
 const rootElement = document.getElementById('root');
